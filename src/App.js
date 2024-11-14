76c17c810bb6a88472582ef6fc1aab931d52a54a
BeDauPro <tbnduc2k3@gmail.com>
2024-11-14 15:18:15 +0700

    fisrtcommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      hello 
+      <Navigation />
+      <Header />
+      <Overview />
+      <Training />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <header className="header">
+      Kỉ niệm 30 năm 
+    </header>
+  );
+}
+
+export default Header;
